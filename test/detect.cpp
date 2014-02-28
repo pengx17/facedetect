549a891e5f9d270f575e4db6d3d02816738c8f1d
peng xiao <hisenxpress@gmail.com>
2014-02-28 11:59:36 +0800

    add y4m video support

--- test/detect.cpp
+++ test/detect.cpp
@@ -6,6 +6,7 @@
 #include "opencv2/opencv.hpp"
 #include "facedetect.h"
 
+const int iterations = 100;
 
 // YUV420 videos can be downloaded at http://trace.eas.asu.edu/yuv/.
 // please rename video name to *.widthxheight.I420
@@ -13,7 +14,7 @@ static void help()
 {
     std::cout << "Usage: ./detect img.I420" << std::endl;
     std::cout << "  support image formats defined in cv::imread and I420" << std::endl;
-    std::cout << "  support I420 YUV videos" << std::endl;
+    std::cout << "  support I420 YUV and 420 y4m videos" << std::endl;
 }
 
 //I420 image is customized as:
@@ -54,6 +55,7 @@ static void writeI420Image(const cv::Mat &src, const std::string& filename)
     fout.close();
 }
 
+//for I420 video without headers
 static bool constructMatFromString(const char *fpath, cv::Mat &img)
 {
     int height = 0, width = 0;
@@ -72,7 +74,38 @@ static bool constructMatFromString(const char *fpath, cv::Mat &img)
     return true;
 }
 
-static void detectAndShow(const cv::Mat &src, fd::ImageFormat format, int wait = 0)
+static bool loadY4MHeader(std::fstream& fs, cv::Mat &img)
+{
+    fs.seekg(fs.beg);
+    if (fs.good())
+    {
+        char inbuf [256];
+        fs >> inbuf;
+        if (strcmp(inbuf, "YUV4MPEG2") != 0)
+        {
+            return false;
+        }
+        fs.get(); // space
+        int width, height;
+        char c = fs.get();
+        if (c != 'W')
+        {
+            return false;
+        }
+        fs >> width;
+        c = fs.get();// space
+        c = fs.get();
+        if (c != 'H')
+        {
+            return false;
+        }
+        fs >> height;
+        img.create(height * 3 / 2, width, CV_8UC1);
+    }
+    return true;
+}
+
+static double detectAndShow(const cv::Mat &src, fd::ImageFormat format, int wait = 0)
 {
     cv::Mat src_bgr;
     if (format == fd::I420)
@@ -85,7 +118,9 @@ static void detectAndShow(const cv::Mat &src, fd::ImageFormat format, int wait =
     }
     int width = src_bgr.cols, height = src_bgr.rows;
     cv::Mat bitmap(src_bgr.rows, src_bgr.cols, CV_8UC1);
+    double detect_time = (double)cv::getTickCount();
     fd::detectBitmap(src.ptr<void>(), fd::FDSize(width, height), bitmap.ptr<void>(), false, format);
+    detect_time = (cv::getTickCount() - detect_time) / cv::getTickFrequency() * 1000.;
     cv::imshow("src", src_bgr);
     cv::imshow("bitmap", bitmap);
     cv::Mat clip;
@@ -93,15 +128,47 @@ static void detectAndShow(const cv::Mat &src, fd::ImageFormat format, int wait =
     cv::bitwise_and(bitmap, src_bgr, clip);
     cv::imshow("clip", clip);
     cv::waitKey(wait);
+    return detect_time;
+}
+
+// just show the output
+static void show(const cv::Mat &src, fd::ImageFormat format, int wait = 0)
+{
+    cv::Mat src_bgr;
+    if (format == fd::I420)
+    {
+        cv::cvtColor(src, src_bgr, CV_YUV2BGR_I420);
+    }
+    else
+    {
+        src_bgr = src;
+    }
+    int width = src_bgr.cols, height = src_bgr.rows;
+    cv::imshow("src", src_bgr);
+    cv::waitKey(wait);
 }
 
 // read a frame from input fstream
 // frame width and height is needed to be given in the img
-static bool readI420Video(std::fstream& fs, cv::Mat &img)
+static bool readI420Video(std::fstream& fs, cv::Mat &img, bool isY4M = false)
 {
     if(!fs.good())
     {
         return false;
+    }
+    if (isY4M)
+    {
+        // skip to next frame start
+        char buf[256] = "";
+        while(strcmp(buf, "FRAME") != 0)
+        {
+            fs >> buf;
+        }
+        char c = fs.get();
+        while (c != 0x0A)
+        {
+            c = fs.get();
+        }
     }
     fs.read((char *)img.data, img.rows * img.cols);
     return true;
@@ -115,23 +182,31 @@ int main(int argc, char **argv)
         return 0;
     }
     fd::ImageFormat format = strstr(argv[1], "I420") != 0 ? fd::I420 : fd::BGR;
-    bool isI420Video = strstr(argv[1], "I420.yuv") != 0;
+    bool isI420YUV = strstr(argv[1], "I420.yuv") != 0;
+    bool isI420Y4M = strstr(argv[1], ".y4m") != 0;
     cv::Mat src, src_bgr;
-    
-    if (isI420Video)
+    std::fstream fs;
+    if (isI420YUV || isI420Y4M)
     {
-        std::fstream fs(argv[1], std::fstream::in | std::fstream::binary);
+        format = fd::I420;
+        fs = std::fstream(argv[1], std::fstream::in | std::fstream::binary);
         if (!fs.good())
         {
             std::cout << "Error: cannot open input video!" << std::endl;
         }
-        if (!constructMatFromString(argv[1], src))
+        if (isI420YUV)
         {
-            std::cout << "Error: cannot get valid frame size from file name!" << std::endl;
+            if (!constructMatFromString(argv[1], src))
+            {
+                std::cout << "Error: cannot get valid frame size from file name!" << std::endl;
+            }
         }
-        while(readI420Video(fs, src))
+        else if (isI420Y4M)
         {
-            detectAndShow(src, format, 30);
+            if (!loadY4MHeader(fs, src))
+            {
+                std::cout << "Error: cannot get valid frame size from file name!" << std::endl;
+            }
         }
     }
     else
@@ -149,7 +224,18 @@ int main(int argc, char **argv)
             std::cout << "Error: cannot open input image!" << std::endl;
             return 0;
         }
-        detectAndShow(src, format);
     }
+    double total_time = 0;
+    int frames = 0;
+    int wait_time = iterations > 0 ? 30 : 0;
+    while(((isI420YUV || isI420Y4M) && readI420Video(fs, src, isI420Y4M)) || (frames < iterations))
+    {
+        double detect_time = detectAndShow(src, format, wait_time);
+        total_time += detect_time;
+        std::cout << frames << ": " << detect_time << " ms" << std::endl; 
+        frames ++;
+        //show(src, format, wait_time);
+    }
+    std::cout << "average detection time " << total_time / frames << " ms" << std::endl;
     return 0;
 }
